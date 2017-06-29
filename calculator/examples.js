83b4af3c03cef3c5ba79d95ebba54994410ddd7f
alezvi <alezvi@users.noreply.github.com>
2017-06-29 12:33:43 -0300

    Update examples.js
    Examples of usage for Calc.js

--- calculator/examples.js
+++ calculator/examples.js
@@ -1,1 +1,18 @@
 
+// Operation concatenation
+Calc.add(5).mul(5).div(2).sub(1);
+
+// Get the final result
+Calc.getResult();
+
+// Undo the last step
+Calc.undo();
+
+// Perform another operation
+Calc.mul(3);
+
+// Get the new result
+Calc.getResult();
+
+// Reset the result
+Calc.reset();
