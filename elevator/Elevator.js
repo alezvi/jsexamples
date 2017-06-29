21d94ede3220422ecb5cb094724fe869a5b1f806
alezvi <alezvi@users.noreply.github.com>
2017-06-29 12:37:34 -0300

    Create Elevator.js

---
+++ elevator/Elevator.js
@@ -0,0 +1,26 @@
+var Elevator = {
+
+  maxWeightAllowed : 225,
+
+  floors : [],
+  
+  selectedFloors : [],
+  
+  bips : 3,
+  
+  alerts : {
+    'exceed' : '',
+    'door_open' : '',
+  },
+  
+  selectFloor : function(floor) {},
+  
+  up : function () {},
+  
+  down : function () {},
+  
+  stop : function () {},
+  
+  closeDoor : function () {},
+  
+}
