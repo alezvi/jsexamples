7f17660be847ed997e7e07511f077695a7f90559
alezvi <alezvi@users.noreply.github.com>
2017-06-29 12:27:33 -0300

    Create calc.js
    Calculator example in a procedural way.

---
+++ calc.js
@@ -0,0 +1,20 @@
+
+var initialValue = 0;
+
+var history = [];
+
+function add(v) {
+    initialValue += v;
+}
+
+function sub(v) {
+    initialValue -= v;
+}
+
+function mul(v) {
+    initialValue *= v;
+}
+
+function div(v) {
+    initialValue /= v;
+}
