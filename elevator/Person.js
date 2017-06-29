44820e032cbbe7da6c18a6cecb91b09224d48c31
alezvi <alezvi@users.noreply.github.com>
2017-06-29 12:28:18 -0300

    Create Person.js

---
+++ elevator/Person.js
@@ -0,0 +1,14 @@
+var Person = {
+  
+    weigth : 0,
+    
+    setWeight : function (kg) {
+        this.weigth = kg;
+        return this;
+    },
+    
+    getWeight : function () {
+        return this.weigth;
+    }
+    
+}
