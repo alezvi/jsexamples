d8fd74f517ac0f36eb497969a6e8231408217d07
alezvi <alezvi@users.noreply.github.com>
2017-06-29 12:26:03 -0300

    Create Calc.js

---
+++ Calc.js
@@ -0,0 +1,42 @@
+var Calc = {
+    history : [],
+    result : 0,
+
+    add : function (v) {
+        this.result += v;
+        return this;
+    },
+    
+    sub : function (v) {
+        this.result -= v;
+        return this;
+    },
+
+    mul : function (v) {
+        this.result *= v;
+        return this;
+    },
+
+    div : function (v) {
+        if (v == 0) {
+            throw Error('Division by zero!');
+        }
+        
+        this.result /= v;
+        return this;
+    },
+
+    result : function () {
+        return this.history[this.history.length-1];
+    },
+
+    reset : function () {
+        this.results = [];
+        return this;
+    },
+    
+    undo : function () {
+		this.history[this.history.length-1] = null;
+        return this;
+    }
+}
