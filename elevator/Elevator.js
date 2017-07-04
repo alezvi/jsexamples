35780f80033a74b9f1f555c46ff2f75a96f4d1f0
alezvi <alezvi@users.noreply.github.com>
2017-07-04 00:27:46 -0300

    Update Elevator.js

--- elevator/Elevator.js
+++ elevator/Elevator.js
@@ -1,6 +1,8 @@
 var Elevator = {
 
   maxWeightAllowed : 225,
+  
+  maxOccupantsAllowed : 3,
 
   floors : [],
   
@@ -8,12 +10,36 @@ var Elevator = {
   
   bips : 3,
   
+  occupants : [],
+  
   alerts : {
     'exceed' : '',
     'door_open' : '',
   },
   
   selectFloor : function(floor) {},
+  
+  addPeople : function (person) {
+      if (this.isFull()) return false;
+      
+      if (person instanceof Person) {
+          this.occupants.push(Person);
+      }
+    
+      return this;
+  },
+  
+  isFull : function () {
+      return this.occupants.length == 3;
+  },
+  
+  isOver : function () {
+      var weight += this.occupants.map(function(person) {
+          return person.weight;
+      })
+    
+      return weigth > this.maxWeightAllowed;
+  },
   
   up : function () {},
   
